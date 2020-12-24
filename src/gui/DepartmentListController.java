c3b1080e1c20bd8438eeff32f1b45e7a1b2dac1a
Tony Silva <tony.music7@gmail.com>
2020-12-24 11:03:11 -0300

    Criada DepartmentService e refatorada loadView com novos parms

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,38 +15,57 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
 
+	private DepartmentService service; // Classe de serviços
+	private ObservableList<Department> obsList; // lista p/ carregar os departamentos
+	
 	@FXML
-	private TableView<Department> tableViewDepartment;
+	private TableView<Department> tableViewDepartment; // tabela
 	@FXML
-	private TableColumn<Department, Integer> tableColumnId;
+	private TableColumn<Department, Integer> tableColumnId; // coluna id
 	@FXML
-	private TableColumn<Department, String> tableColumnName;
+	private TableColumn<Department, String> tableColumnName; // coluna nome 
 	@FXML
 	private Button btNew;
 	
+	// comportamento do botão "Novo"
 	@FXML
 	public void onBtNewAction() {
 		System.out.println("onBtNewAction");
 	}
 	
+	// Inicia antes da tela ser montada
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		initializeNodes(); // personalizado
+		initializeNodes(); // método que permite comportamento correto da tabela
 	}
 
-	// padrão do JavaFX que permite inicializar apropriadamente as colunas
+	// padrão do JavaFX que permite inicializar apropriadamente as colunas da tabela
 	private void initializeNodes() {
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
 		
-		// Macete p/ fazer a table view acompanhar a altura da janela
+		// Macete p/ fazer a tabela acompanhar o dimensionamento da janela
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
-		
-		
+	}
+	
+	// permite acoplamento fraco - (inversão de controle - solid)
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
+	
+	// Acessa, carrega e guarda os departamentos em obsList
+	public void updateTableView() {
+		if(service == null) {
+			throw new IllegalStateException("Serviço estava null");
+		}
+		List<Department> list = service.findAll(); // carrega lista de departamentos
+		obsList = FXCollections.observableArrayList(list); // armazena em obsList
+		tableViewDepartment.setItems(obsList); // carrega na tabela da view
 	}
 
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -15,6 +16,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable {
 	
@@ -32,14 +34,17 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemDeparmentAction() {
-		// Carrega a view Department
-		loadView("/gui/DepartmentList.fxml");
+		// Carrega a view Department com parâmetros
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		});
 	}
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		// Carrega a view About
-		loadView("/gui/About.fxml");
+		// Carrega a view About passando o 2º parâmetro com nada dentro p/ a consumer
+		loadView("/gui/About.fxml", x -> {});
 	}
 
 	@Override
@@ -49,12 +54,13 @@ public class MainViewController implements Initializable {
 	
 	/*
 	 * Abre uma nova view dentro MainView
-	 * 'synchronized' permite que processamento das trades não seja interrompida 
+	 * 'synchronized' permite que processamento das trades não seja interrompida
+	 * Também é uma "Consumer", função "Generic" do tipo <T>
 	 */
-	public synchronized void loadView(String absoluteName) {
+	public synchronized <T> void loadView(String urlView, Consumer<T> consumerExecute) {
 		try {
 			// armazena a url a view passada no parâmetro
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(urlView));
 			// guarda a view do endereço acima
 			VBox newVBox = loader.load();
 			
@@ -69,10 +75,12 @@ public class MainViewController implements Initializable {
 			mainVBox.getChildren().add(mainMenu); // add o menu de volta na MainView
 			mainVBox.getChildren().addAll(newVBox.getChildren()); // add os filhos do novo VBox
 			
+			// Ativa o consumer executando o "Consumer" passado no parâmetro
+			T controller = loader.getController();
+			consumerExecute.accept(controller);
 			
 		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Erro ao carregar a página", e.getMessage(), AlertType.ERROR);
 		}
 	}
-
 }
